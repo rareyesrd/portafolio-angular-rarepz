982147f862aa279784d78c4be76b8a20a48e9416
Rafael Alexander Reyes Pérez <rafaelalexanderr5@gmail.com>
2021-09-29 06:06:50 -0400

    [ADD] Experience

--- src/app/pages/home-page/experience/experience.component.ts
+++ src/app/pages/home-page/experience/experience.component.ts
@@ -3,13 +3,58 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.component.html',
-  styleUrls: ['./experience.component.scss']
+  styleUrls: ['./experience.component.scss'],
 })
 export class ExperienceComponent implements OnInit {
+  profesionalExperience = [
+    {
+      time: 'Since Aug 2021',
+      position: 'UI User Interface Manager,',
+      place: 'Refactoring.do',
+      typeOfWork: 'Remote',
+      description:
+        'Managing the design and development of web-based technical solutions that engage users and meet business requirements.',
+    },
 
-  constructor() { }
+    {
+      time: 'Since Jan 2021',
+      position: 'Front end Developer,',
+      place: 'Caribe Media Inc',
+      typeOfWork: 'Presential',
+      description:
+        'Working on a web app project in a highly collaborative work environment using Scrum and Angular 11+ for development.',
+    },
 
-  ngOnInit(): void {
-  }
+    {
+      time: 'May 2019 – Jan 2021',
+      position: 'Web designer,',
+      place: 'Caribe Media Inc',
+      typeOfWork: 'Presential',
+      description:
+        'Performing as a creator of web pages, understanding the needs of clients and translating them digitally.',
+    },
+  ];
 
+  educationalExperience = [
+    {
+      time: 'Since 2016',
+      title: 'Computer engineering',
+      place: 'Technological University of Santiago (UTESA).',
+    },
+
+    {
+      time: '2017-2018',
+      title: 'English Immersion Program',
+      place: 'Universidad Acción Pro-Educación y Cultura (UNAPEC).',
+    },
+
+    {
+      time: '2017',
+      title: 'AP Computer Science Principles',
+      place: 'Online at CodeHS, Udemy, Coursera, Codecademy and YouTube.',
+    },
+  ];
+  constructor() {}
+
+  ngOnInit(): void {}
 }
